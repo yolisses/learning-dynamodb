f73693bcbdb3967aebe164146ba7e96f0bfd8f8f
Ulisses <m.stunik@gmail.com>
2021-09-13 21:01:25 -0300

    add createPlant method

---
+++ src/schema.js
@@ -0,0 +1,49 @@
+export const schema = `
+type User {
+  id: ID! 
+  name: String
+  email: String
+}
+
+input UserInput {
+  name: String
+  email: String
+}
+
+type Plant{
+  name: String!
+  id: ID!
+  userId: ID!
+  images: [String!]!
+  type: String!
+  tags: [String!]
+  description: String
+  donate: Boolean
+  swap: Boolean
+  sell: Boolean
+  price: Float
+  amount: Int
+}
+
+input PlantInput{
+  name: String!
+  images: [String!]!
+  type: String!
+  tags: [String!]
+  description: String
+  donate: Boolean
+  swap: Boolean
+  sell: Boolean
+  price: Float
+  amount: Int
+}
+
+type Query {
+  getUser(id: ID!): User
+  getPlant(id: ID!): Plant
+}
+
+type Mutation {
+  createUser(input: UserInput): User
+  createPlant(input: PlantInput): Plant
+}`;
